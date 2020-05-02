5597a1fa17ffd8b527916ce534084c7f0633d644
Rosemary <rosemary@localhost.localdomain>
2020-05-02 14:38:30 -0400

    initial commit

---
+++ appfront/src/components/HelloWorld.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <div v-for="item in items.items" :key="item.id">
+      <h1>{{item.title}}</h1>
+      <p>{{item.firstname}}</p>
+      <p>{{item.surname}}</p>
+      <p>{{item.address}}</p>
+      <p>{{item.id}}</p>
+      <p>{{item.type}}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  name: 'HelloWorld',
+  props: {
+  },
+  data: function() {
+    return {
+      items: []
+    }
+  },
+  mounted () {
+    axios.get('http://localhost:3000/items')
+      .then((response) => {
+        this.items = response.data
+        });
+  }
+}
+</script>
+
+<style scoped>
+</style>
